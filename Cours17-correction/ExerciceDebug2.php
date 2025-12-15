7d241b5c17abde909d7337e207661a4a4ef8798c
Guillaume Harvey <guillaumeharv@gmail.com>
2025-12-15 16:05:38 -0500

    Bonnes réponses dans les corrections / cours 17

--- Cours17-correction/ExerciceDebug2.php
+++ Cours17-correction/ExerciceDebug2.php
@@ -14,23 +14,26 @@
     while($troisFoisSix != true)
     {
         $nbEssais++;
+        //5. ré-initialiser le nombre de fois qu'on a 6 à zéro, à chaque fois qu'on lance les 3 dés
+        $nbFoisSix = 0;
 
         //premier lancer
         $lancer1 = rand(1,6);
-        if($lancer1 = 6)
+        //var_dump($lancer1);
+        if($lancer1 == 6) //2. il fallait mettre le double égal pour la comparaison et non le simple égal
             $nbFoisSix++;
         
         //deuxieme lancer
         $lancer2 = rand(1,6);
-        if($lancer2 = 6)
+        if($lancer2 == 6) //3. il fallait mettre le double égal pour la comparaison et non le simple égal
             $nbFoisSix++;
 
         //troisieme lancer
         $lancer3 = rand(1,6);
-        if($lancer3 = 6)
+        if($lancer3 == 6) //4. il fallait mettre le double égal pour la comparaison et non le simple égal
             $nbFoisSix++;
 
-        echo "Voici les lancers : $lancer1, $lancer2, $lancer3<br>"
+        echo "Voici les lancers : $lancer1, $lancer2, $lancer3<br>"; //1. erreur de syntaxe, le point-virgule
 
         //si les trois lancers sont 6
         if($nbFoisSix == 3)
